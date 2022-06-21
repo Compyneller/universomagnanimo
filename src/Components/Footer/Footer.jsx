d8dcd8dc22187c7534d7dc59cbbcc1d467a022f6
Ronin <compyneller@gmail.com>
2022-06-21 15:10:58 +0530

    add social links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -18,12 +18,43 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <a
-          className="my-auto text-center emailLink"
-          href="mailto:hello@universomagnanimo.com"
-        >
-          <h2 className="text-dark ">hello@universomagnanimo.com</h2>
-        </a>
+        <div className="row w-100" style={{ zIndex: "1000" }}>
+          <a
+            className="my-auto text-center emailLink"
+            href="mailto:hello@universomagnanimo.com"
+          >
+            <h2 className="text-dark ">hello@universomagnanimo.com</h2>
+          </a>
+        </div>
+        <div className="row w-100" style={{ zIndex: "1000" }}>
+          <div className="col-4 col-lg-2">
+            <a
+              style={{ color: "white", fontSize: "40px" }}
+              href="https://www.linkedin.com/company/universo-magnanimo-unipessoal-lda/"
+              target="_blank"
+            >
+              <i className="fa-brands fa-linkedin" />
+            </a>
+          </div>
+          <div className="col-4 col-lg-2">
+            <a
+              style={{ color: "white", fontSize: "40px" }}
+              href="https://twitter.com/UniversoMagnan1"
+              target="_blank"
+            >
+              <i className="fa-brands fa-twitter" />
+            </a>
+          </div>
+          <div className="col-4 col-lg-2">
+            <a
+              style={{ color: "white", fontSize: "40px" }}
+              href="https://medium.com/me/stories/public"
+              target="_blank"
+            >
+              <i className="fa-brands fa-medium"></i>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
